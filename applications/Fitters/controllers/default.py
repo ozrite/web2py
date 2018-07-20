5c38fc4ec641ba23e25ff40e9c0c545697cbde5b
Asad Rashid <asad@Asads-MacBook-Pro.local>
2018-07-21 02:21:56 +0500

    fitters fromt page layout complete yay

--- applications/Fitters/controllers/default.py
+++ applications/Fitters/controllers/default.py
@@ -17,6 +17,7 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
+    page = 'home'
     if session.a is None:
         session.a = 1
     else:
@@ -24,7 +25,8 @@ def index():
 
     # response.flash = T("Hello World")
     # response.flash = session.a
-    return dict(message=T('Welcome to web2py!'))
+    # return dict(message=T('Welcome to web2py!'))
+    return locals()
 
 def test():
 #     return "<h2>hello test</h2>"
@@ -40,6 +42,7 @@ def subview1():
     return locals()
 
 def men():
+    page = 'men'
     msg = "welcome to Fitters Men"
     name = request.vars.your_name
     # redirect(URL('women'))
@@ -48,11 +51,13 @@ def men():
     return locals()
 
 def women():
+    page = 'women'
     # x = request.args
     msg = "welcome to Fitters Women"
     return locals()
 
 def mini():
+    page = 'mini'
     msg = "welcome to Fitters mini"
     # redirect(URL('men'))
     form = SQLFORM.factory(Field('your_name')).process()
@@ -61,6 +66,7 @@ def mini():
     return locals()
 
 def unstitched():
+    page = 'unstitched'
     #msg = "welcome to unstitched"
     # form = SQLFORM(db.blog_post).process()
     # rows = db(db.blog_post).select()
@@ -68,60 +74,74 @@ def unstitched():
     return locals()
 
 def accessories():
+    page = 'accessories'
     msg = "welcome to accessories"
     return locals()
 
 def business():
+    page = 'business'
     msg = "welcome to Fitters Business"
     return locals()
 
 def overseas():
+    page = 'overseas'
     msg = "welcome to Fitters Overseas"
     return locals()
 
 def prices():
+    page = 'prices'
     msg = "welcome to prices"
 
     yargs = request.args(0)
     return locals()
 
 def offers():
+    page = 'offers'
     msg = "welcome to offers"
     return locals()
 
 def quickorder():
+    page = 'quickorder'
     msg = "welcome to Quick Order"
     return locals()
 
 def about():
+    page = 'about'
     msg = "welcome to AboutUs"
     return locals()
 
 def contact():
+    page = 'contact'
     msg = "welcome to contactus"
     return locals()
 
 def videos():
-    msg = "welcome to videos"
+    page = 'videos'
+    msg = "videos"
     return locals()
 
 def jobs():
+    page = 'jobs'
     msg = "welcome to jobs"
     return locals()
 
 def faq():
+    page = 'faq'
     msg = "welcome to faq"
     return locals()
 
 def privacy():
+    page = 'privacy'
     msg = "welcome to privacy"
     return locals()
 
 def terms():
+    page = 'terms'
     msg = "welcome to terms"
     return locals()
 
 def cancelation():
+    page = 'cancelation'
     msg = "welcome to cancelation"
     return locals()
 
